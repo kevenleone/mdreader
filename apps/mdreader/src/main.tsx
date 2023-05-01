f9929e76a8678e31da1b26e48d49271faf6b0764
Keven Leone <keven.leone@liferay.com>
2023-04-30 22:46:14 -0300

    Add SWR #16

--- apps/mdreader/src/main.tsx
+++ apps/mdreader/src/main.tsx
@@ -1,10 +1,31 @@
+import { SWRConfig } from 'swr';
 import ReactDOM from 'react-dom/client';
+
 import MDReaderRouter from './Router.tsx';
 
 import './index.css';
-import '@mdreader/ui/styles/ui.css';
 import '@mdreader/md/styles/md.css';
+import '@mdreader/ui/styles/ui.css';
+
+function localStorageProvider() {
+  const map = new Map(JSON.parse(localStorage.getItem('app-cache') || '[]'));
+
+  window.addEventListener('beforeunload', () => {
+    const appCache = JSON.stringify(Array.from(map.entries()));
+    localStorage.setItem('app-cache', appCache);
+  });
+
+  return map as any;
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <MDReaderRouter />
+  <SWRConfig
+    value={{
+      refreshInterval: 0,
+      provider: localStorageProvider,
+      revalidateOnFocus: false,
+    }}
+  >
+    <MDReaderRouter />
+  </SWRConfig>
 );
